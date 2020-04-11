8b8d263b54958458796cd74fa57fa4f325d12b17
Damon <voldev@hotmail.com>
2020-04-10 21:43:11 -0400

    create superuser, register models with admin, adding data(realtors and listings), customize admin ui

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Listing
+
+
+class ListingAdmin(admin.ModelAdmin):
+    # displaying columns on admin page
+    list_display = ('id', 'title', 'is_published',
+                    'price', 'list_date', 'realtor')
+    # clickable links
+    list_display_links = ('id', 'title')
+    # filter option
+    list_filter = ('realtor',)
+    list_editable = ('is_published',)
+    search_fields = ('title', 'description', 'address',
+                     'city', 'state', 'zipcode')
+    list_per_page = 25
+
 
 # Register your models here.
+admin.site.register(Listing, ListingAdmin)
