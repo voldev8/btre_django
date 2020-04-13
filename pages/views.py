4fb4faae37429249b268ef56e25c0cdbb4ba4d96
Damon <voldev@hotmail.com>
2020-04-13 18:04:33 -0400

    single Listing page dynamic, search form dynamic and filter

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from listings.choices import bedroom_choices, price_choices, state_choices
+
 from listings.models import Listing
 from realtors.models import Realtor
 
@@ -10,7 +12,10 @@ def index(request):
         '-list_date').filter(is_published=True)[:3]
 
     context = {
-        'listings': listings
+        'listings': listings,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+        'state_choices': state_choices,
     }
     return render(request, 'pages/index.html', context)
 
