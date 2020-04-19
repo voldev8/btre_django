d8e0a508781f83b1f85d1307d056490b4bd1fa10
Damon <voldev@hotmail.com>
2020-04-19 11:07:25 -0400

    send email dashboard functionality

--- btre/settings.py
+++ btre/settings.py
@@ -141,3 +141,10 @@ MEDIA_URL = '/media/'
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+# Email config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'voldev8@gmail.com'
+EMAIL_HOST_PASSWORD = 'woWebdev33!'
+EMAIL_USE_TLS = True
